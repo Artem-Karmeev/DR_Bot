a5dde1ff52c0308a01a763faffad8fcac114292d
Artem Karmeev <skinnynew161@gmail.com>
2023-03-13 21:46:01 +0300

    добавление, удаление, поиск002

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 from aiogram import Dispatcher, Bot
 
-bot = Bot(token='5688981493:AAEVZtJ4DFiQ7dH7tmKcXj8Ze3I8RcSCeAU')
+bot = Bot(token='')
 dp = Dispatcher(bot)
-admin_id = '5076430555'
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from handlers import dp
 from aiogram import executor
-from config import bot
 
 
 async def startup(_):
     print('Hello!')
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=startup)
